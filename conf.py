04e8f39c9abcd73a3621cd9b734ecc58f6420618
beyond.li <letscan@gmail.com>
2017-10-09 20:51:15 +0800

    tweak css, mainly for exercises and code

--- conf.py
+++ conf.py
@@ -102,7 +102,9 @@ todo_include_todos = False
 if on_rtd:
     html_theme = 'default'
 else:
-    html_theme = 'sphinxdoc'
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_show_sourcelink = False
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -115,6 +117,7 @@ html_show_sourcelink = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_style = 'my.css'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
