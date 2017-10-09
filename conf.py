16d0296a0e8b9af5b63d947cea2c17bf47e31f1a
beyond.li <letscan@gmail.com>
2017-10-09 21:48:03 +0800

    rollback custom css

--- conf.py
+++ conf.py
@@ -115,7 +115,7 @@ html_show_sourcelink = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_style = 'my.css'
+#html_style = 'my.css'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
