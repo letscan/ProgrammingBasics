d63f3342c2b141f4ba70a937512aaf29acf4f319
beyond.li <letscan@gmail.com>
2017-11-07 22:16:11 +0800

    fix relative path

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ else:
 graphviz_output_format = 'svg'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['../_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -122,7 +122,7 @@ html_show_sourcelink = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
